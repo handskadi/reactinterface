4487798ccdf2c02420c89840ff30ffd6acb79195
Mohamed KADI <handskadi@gmail.com>
2024-09-22 18:39:00 +0200

    init + tailwind integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { BiCalendar } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from "./components/AppointmentInfo";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [appointentList, setAppointentList] = useState([]);
+  const [query, setQuery] = useState("");
+  const filterAppointments = appointentList.filter((item) => {
+    return (
+      item.petName.toLowerCase().includes(query.toLowerCase()) ||
+      item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+      item.aptNotes.toLowerCase().includes(query.toLowerCase())
+    );
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/data.json")
+      .then((response) => response.json())
+      .then(setAppointentList);
+  }, [setAppointentList]);
+
+  function handleDeletion(appointmentId) {
+    setAppointentList(
+      appointentList.filter((appointment) => appointment.id !== appointmentId)
+    );
+  }
+
+  function handleSearch(myQuery) {
+    setQuery(myQuery);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top" />
+        Your Appointments
+      </h1>
+      <AddAppointment />
+      <Search onQueryChange={handleSearch} query={query} />
+
+      <ul className="divide-y divide-gray-200">
+        {filterAppointments.map((appointment) => (
+          <AppointmentInfo
+            key={appointment.id}
+            appointment={appointment}
+            onDeleteAppointment={handleDeletion}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
